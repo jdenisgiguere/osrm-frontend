c98a59512edba5b3f200e7a0acae30b2c8ea1846
Jean-Denis Giguere <jdenisgiguere@gmail.com>
2016-10-19 10:11:03 -0400

    mouseover information about charging (syntax update)

--- src/index.js
+++ src/index.js
@@ -94,14 +94,12 @@ function makeIcon(i, n) {
 
 //setup charging popup
 var chargingPopup = function(charging_step) {
-
   var chargingDurationInMinutes = (charging_step.charging_duration / 60.0).toFixed(0);
   var content = "<h3>" + charging_step.name + "</h3>" + "<p>Durée de la recharge : " + chargingDurationInMinutes +
       " minutes</p>";
 
   return content;
 };
-
 
 var plan = new ReversablePlan([], {
   geocoder: Geocoder.nominatim(),
@@ -111,7 +109,6 @@ var plan = new ReversablePlan([], {
       draggable: this.draggableWaypoints,
       icon: makeIcon(i, n)
     };
-    //wp.popupContent = "Hello";
     var marker = L.marker(wp.latLng, options);
     marker.on('click', function() {
       plan.spliceWaypoints(i, 1);
@@ -224,7 +221,7 @@ plan.on('waypointdragend', function(e) {
           var lon = steps[i]["location"][0];
           var lat = steps[i]["location"][1];
           var chargerWP = makeWaypoint(lat, lon);
-          chargerWP.popupContent = chargingPopup(steps[i]); // + "/n Durée de la recharge : " + (steps[i].duration / 60.0).toFixed(0) + " minutes";
+          chargerWP.popupContent = chargingPopup(steps[i]);
           chargerWP.is_charger = true;
           newWps.push(chargerWP);
         }
